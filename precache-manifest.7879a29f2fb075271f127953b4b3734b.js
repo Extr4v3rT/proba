44f5b3766785f213299923e12f5c4bf7f994f3b9
Dawid Kobielski <dawidkobielski@MacBook-Pro-Dawid.local>
2020-07-28 13:32:15 +0200

    Updates

---
+++ precache-manifest.7879a29f2fb075271f127953b4b3734b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ce7abd104c11cd13181619d830853d",
+    "url": "/proba/index.html"
+  },
+  {
+    "revision": "c417fb6589a4361a5285",
+    "url": "/proba/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3cd7fc42a0fe19fb4909",
+    "url": "/proba/static/js/2.e52594f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proba/static/js/2.e52594f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c417fb6589a4361a5285",
+    "url": "/proba/static/js/main.b29322c7.chunk.js"
+  },
+  {
+    "revision": "91a7e16c21782701c78d",
+    "url": "/proba/static/js/runtime-main.0b668a3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proba/static/media/logo.5d5d9eef.svg"
+  }
+]);
